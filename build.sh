0bb43129ed4e38207aa80af79bc28adde0132a19
Tiger Wang <tigerwang@outlook.com>
2022-11-14 10:19:19 -0500

    wip

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-BUILD_DIR=$(dirname "${BASH_SOURCE[0]}")/dist/demo_js_wasm
+BUILD_DIR=$(dirname "${BASH_SOURCE[0]}")/build
 
-cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" "$BUILD_DIR"
+cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" "$BUILD_DIR"/www/
